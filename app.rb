e175883e2e72a2fb588fdccb9ab177448fb011f4
Jed Peek <jedpeek@yahoo.com>
2018-07-24 14:42:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,4 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  post '/student' do
+    <form action="/student" method="post">
+      Student Name: <input type="text" name="student[name]">
+      Student Grade: <input type="text" name="student[grade]">
+      <input type="submit">
+    </form>
+  end
+
 end
